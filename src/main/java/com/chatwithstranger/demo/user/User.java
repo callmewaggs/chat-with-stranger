6d5f42fc5b21de4739007e6f568c2e4c582628df
callmewaggs <zxv74014@gmail.com>
2019-09-05 12:59:58 +0900

    Model - User 생성 signup page 작성중 background 수정 jdbc 연결 및 jpa 추가

---
+++ src/main/java/com/chatwithstranger/demo/user/User.java
@@ -0,0 +1,42 @@
+package com.chatwithstranger.demo.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String lastLogin;
+
+    private User() {
+    }
+
+    private User(String username, String password, String lastLogin) {
+        this.username = username;
+        this.password = password;
+        this.lastLogin = lastLogin;
+    }
+
+    public static User createWithLastLogin(String username, String password, String lastLogin) {
+        return new User(username, password, lastLogin);
+    }
+
+    public static User createWithoutLastLogin(String username, String password) {
+        return new User(username, password, null);
+    }
+
+    public static User createInitialUser() {
+        return new User(null, null, null);
+    }
+}
